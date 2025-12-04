337bc908f5de1e55a7efe09e3553bceec14c1b3a
neelchauhaniugfkja <neel.chauhan113@gmail.com>
2025-12-04 09:12:10 +0800

    firstfullcommit

---
+++ lukas minigame/bigbutton.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var pressed_needed = randi_range(10, 30)
+var main : String = "res://node_2d.tscn"
+var game_finished = false
+@onready var press_label = $TextEdit
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	press_label.text = "Presses needed: " + str(pressed_needed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if pressed_needed == 0 and game_finished == false:
+		end_game()
+		
+
+
+func _on_button_pressed() -> void:
+	set_count(pressed_needed - 1)
+	print(pressed_needed)
+	
+func set_count(new_need_presses: int) -> void:
+	pressed_needed = new_need_presses
+	press_label.text = "Presses needed: " + str(pressed_needed)
+
+func end_game() -> void:
+	print('Neel')
+	var	scene = load(main)
+	get_tree().change_scene_to_packed(scene)
+	game_finished = true
---
+++ node_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+var server_ip := "10.184.1.68"
+var role := "weapons"
+var team := "tech"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#SpaceApi.server_connect(server_ip, role, team)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
